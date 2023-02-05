24ed97278be313494ebb3658f6483a8a63a5089e
simany27 <simany27@gmail.com>
2023-02-05 18:09:24 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+import telebot
+from config import keys, TOKEN
+from extensions import APIException, CryptoConverter
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def greeting(message):
+    text = 'Чтобы начать работу, введите запрос в следующем формате: ' \
+           '\n"<валюта из которой перевести> <валюта в которую перевести> <количество>"' \
+           '\nУвидеть список доступных валют: /values'
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message):
+    text = 'Доступные валюты: '
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text'])
+def convert(message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise APIException('Слишком много параметров')
+
+        quote, base, amount = values
+        total_base = CryptoConverter.get_price(quote, base, amount)
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e} ')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} = {total_base}'
+        bot.send_message(message.chat.id, text)
+
+bot.polling(none_stop=True)
---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = "5770246199:AAFHgru9SkcizDpShTtSo1K7gj6wRzWXpcc"
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
